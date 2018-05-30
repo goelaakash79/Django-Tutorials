21b59a37687dabc91bd9b991d3dfc4acaae962fc
goelaakash79 <goelaakash79@gmail.com>
2018-05-31 02:12:12 +0530

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
 
+import json
+from django.http import JsonResponse
 from .models import Question
 
 # Create your views here.
@@ -18,16 +20,27 @@ def index(request):
     # return render(request, 'polls/index.html', context)
 
 def about(request):
-    response = "<h2>This is about section</h2>"
-    return HttpResponse(response)
+    # response = "<h2>This is about section</h2>"
+    # return HttpResponse(response)
+    context = {
+        'name':'Aakash Goel',
+        'rollno':'1620CS1001',
+        'pos':'developer'
+    }
+    return JsonResponse(context)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     # return HttpResponse("You are looking at question %s." % question_id)
+#     context = {'question':question}
+#     return render(request, 'polls/detail.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    # return HttpResponse("You are looking at question %s." % question_id)
-    context = {'question':question}
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
